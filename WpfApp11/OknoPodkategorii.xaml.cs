533512c8dc2cf49e1b56bdbbc67842037a97d240
BartoszL1803 <bartek.lissowski@gmail.com>
2022-07-02 16:35:17 +0200

    Dodaj pliki projektów.

---
+++ WpfApp11/OknoPodkategorii.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp11
+{
+    public partial class OknoPodkategorii : Window
+    {
+        public OknoPodkategorii()
+        {
+            InitializeComponent();
+        }
+
+        public OknoPodkategorii(System.Xml.XmlElement kategoria)
+        {
+            DataContext = kategoria;
+            Console.WriteLine(kategoria);
+            InitializeComponent();
+        }
+
+        private void WyświetlModele(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement parent = (FrameworkElement)((FrameworkElement)sender).Parent;
+            ListBox lista = (ListBox)parent.FindName("Marka");
+            new OknoElementów((System.Xml.XmlElement)lista.SelectedItem).Show();
+        }
+    }
+}
